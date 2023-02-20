003032e9c46449474b8171994fabae89beb6dff4
Mayank Raj <mayankrj1224@gmail.com>
2023-02-21 02:02:30 +0530

    fix the server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => {
-  res.send("Server is Live")
+  res.send("Gold Finance App Server is Live")
 })
 
 app.use("/", router);
